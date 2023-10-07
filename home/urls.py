911e345924fab1c9a04c9c7e3b7034664cc262a7
JessiBi <linjing.biloveet@gmail.com>
2023-10-07 19:47:31 +1100

    add new dashboard

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', lambda request: redirect('/login', permanent=False)),
     path('login', views.UserLoginView.as_view(), name='login_view'),
+    path('logout', views.logout_view, name="logout2"),
     path('home', views.homePage, name='home_page'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from admin_volt_pro.forms import RegistrationForm, LoginForm, UserPasswordResetForm, UserPasswordChangeForm, UserSetPasswordForm
 from django.contrib.auth.views import LoginView, PasswordResetView, PasswordChangeView, PasswordResetConfirmView
 from django.contrib.auth import logout
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as django_logout
 
 # Create your views here.
 
 class UserLoginView(LoginView):
   form_class = LoginForm
   template_name = 'login.html'
-  next_page = '/home'
+  next_page = '/dashboard'
 
 def homePage(request):
-    return render(request, "home.html")
+    return render(request, "includes/navigation.html")
+
+def logout_view(request):
+    django_logout(request)
+    return redirect('/login')
+
